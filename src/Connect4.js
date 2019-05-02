b80191874e39de00905f9936e885694efcdb7cde
Connor Kam-Cheong <connorkc42@gmail.com>
2019-05-02 14:51:36 +0100

    Add eslint, refactor ConnectFour to do checks based on board height and length

--- src/Connect4.js
+++ src/Connect4.js
@@ -1,42 +1,44 @@
 class ConnectFour {
-    constructor() {
-        this.inProgress = true;
-        this.winState = false;
-        this.board = [
-            [' ', ' ', ' ', ' ', ' ', ' '],
-            [' ', ' ', ' ', ' ', ' ', ' '],
-            [' ', ' ', ' ', ' ', ' ', ' '],
-            [' ', ' ', ' ', ' ', ' ', ' '],
-            [' ', ' ', ' ', ' ', ' ', ' '],
-            [' ', ' ', ' ', ' ', ' ', ' ']
-        ];
-    }
+  constructor () {
+    this.inProgress = true
+    this.winState = false
+    this.board = [
+      [' ', ' ', ' ', ' ', ' ', ' '],
+      [' ', ' ', ' ', ' ', ' ', ' '],
+      [' ', ' ', ' ', ' ', ' ', ' '],
+      [' ', ' ', ' ', ' ', ' ', ' '],
+      [' ', ' ', ' ', ' ', ' ', ' '],
+      [' ', ' ', ' ', ' ', ' ', ' ']
+    ]
+    this.boardLength = this.board[0].length
+    this.boardHeight = this.board.length
+  }
 
-    selectColumn(columnNumber, player) {
-      for (let i = 5; i >= 0; i--) {
-        if (this.board[i][columnNumber - 1] === ' ') {
-          this.board[i][columnNumber - 1] = player
-          break
-        }
+  selectColumn (columnNumber, player) {
+    for (let i = this.boardHeight - 1; i >= 0; i--) {
+      if (this.board[i][columnNumber - 1] === ' ') {
+        this.board[i][columnNumber - 1] = player
+        break
       }
-      this.checkHorizontalWin(columnNumber)
     }
+    this.checkHorizontalWin(columnNumber)
+  }
 
-    checkHorizontalWin(columnNumber) {
-      for (let row = 5; row >= 0; row--) {
-        let counter = 0
-        for (let column = 0; column < 6; column++) {
-          if (this.board[row][column] === 1) {
-            counter += 1
-            if (counter === 4) {
-              this.winState = true
-            }
-          } else {
-            counter = 0
+  checkHorizontalWin (columnNumber) {
+    for (let row = 0; row < this.boardHeight; row++) {
+      let counter = 0
+      for (let column = 0; column < this.boardLength; column++) {
+        if (this.board[row][column] === 1) {
+          counter += 1
+          if (counter === 4) {
+            this.winState = true
           }
+        } else {
+          counter = 0
         }
       }
     }
+  }
 }
 
-module.exports = ConnectFour;
+module.exports = ConnectFour
--- test/Connect4.test.js
+++ test/Connect4.test.js
@@ -1,102 +1,103 @@
-const Connect4 = require('../src/Connect4.js');
+/* global describe, test, expect */
+const Connect4 = require('../src/Connect4.js')
 
 describe('Connect4', () => {
-    test('starts with an empty board', () => {
-        const connect4 = new Connect4();
+  test('starts with an empty board', () => {
+    const connect4 = new Connect4()
 
-        expect(connect4.board).toEqual([
-            [' ', ' ', ' ', ' ', ' ', ' '],
-            [' ', ' ', ' ', ' ', ' ', ' '],
-            [' ', ' ', ' ', ' ', ' ', ' '],
-            [' ', ' ', ' ', ' ', ' ', ' '],
-            [' ', ' ', ' ', ' ', ' ', ' '],
-            [' ', ' ', ' ', ' ', ' ', ' ']
-        ]);
-    });
+    expect(connect4.board).toEqual([
+      [' ', ' ', ' ', ' ', ' ', ' '],
+      [' ', ' ', ' ', ' ', ' ', ' '],
+      [' ', ' ', ' ', ' ', ' ', ' '],
+      [' ', ' ', ' ', ' ', ' ', ' '],
+      [' ', ' ', ' ', ' ', ' ', ' '],
+      [' ', ' ', ' ', ' ', ' ', ' ']
+    ])
+  })
 
-    test('puts a player token into the last row', () => {
-      const connect4 = new Connect4();
+  test('puts a player token into the last row', () => {
+    const connect4 = new Connect4()
 
-      connect4.selectColumn(1, 1);
-      expect(connect4.board).toEqual([
-          [' ', ' ', ' ', ' ', ' ', ' '],
-          [' ', ' ', ' ', ' ', ' ', ' '],
-          [' ', ' ', ' ', ' ', ' ', ' '],
-          [' ', ' ', ' ', ' ', ' ', ' '],
-          [' ', ' ', ' ', ' ', ' ', ' '],
-          [1, ' ', ' ', ' ', ' ', ' ']
-      ]);
-    });
+    connect4.selectColumn(1, 1)
+    expect(connect4.board).toEqual([
+      [' ', ' ', ' ', ' ', ' ', ' '],
+      [' ', ' ', ' ', ' ', ' ', ' '],
+      [' ', ' ', ' ', ' ', ' ', ' '],
+      [' ', ' ', ' ', ' ', ' ', ' '],
+      [' ', ' ', ' ', ' ', ' ', ' '],
+      [1, ' ', ' ', ' ', ' ', ' ']
+    ])
+  })
 
-    test('puts a player token into a column', () => {
-      const connect4 = new Connect4();
+  test('puts a player token into a column', () => {
+    const connect4 = new Connect4()
 
-      connect4.selectColumn(1, 1);
-      connect4.selectColumn(1, 2);
-      expect(connect4.board).toEqual([
-          [' ', ' ', ' ', ' ', ' ', ' '],
-          [' ', ' ', ' ', ' ', ' ', ' '],
-          [' ', ' ', ' ', ' ', ' ', ' '],
-          [' ', ' ', ' ', ' ', ' ', ' '],
-          [2, ' ', ' ', ' ', ' ', ' '],
-          [1, ' ', ' ', ' ', ' ', ' ']
-      ]);
-    });
+    connect4.selectColumn(1, 1)
+    connect4.selectColumn(1, 2)
+    expect(connect4.board).toEqual([
+      [' ', ' ', ' ', ' ', ' ', ' '],
+      [' ', ' ', ' ', ' ', ' ', ' '],
+      [' ', ' ', ' ', ' ', ' ', ' '],
+      [' ', ' ', ' ', ' ', ' ', ' '],
+      [2, ' ', ' ', ' ', ' ', ' '],
+      [1, ' ', ' ', ' ', ' ', ' ']
+    ])
+  })
 
-    test('game is won if 4 of the same tokens are connected horizontally', () => {
-      const connect4 = new Connect4();
+  test('game is won if 4 of the same tokens are connected horizontally', () => {
+    const connect4 = new Connect4()
 
-      connect4.selectColumn(1, 1);
-      connect4.selectColumn(2, 1);
-      connect4.selectColumn(3, 1);
-      connect4.selectColumn(4, 1);
+    connect4.selectColumn(1, 1)
+    connect4.selectColumn(2, 1)
+    connect4.selectColumn(3, 1)
+    connect4.selectColumn(4, 1)
 
-      expect(connect4.board).toEqual([
-          [' ', ' ', ' ', ' ', ' ', ' '],
-          [' ', ' ', ' ', ' ', ' ', ' '],
-          [' ', ' ', ' ', ' ', ' ', ' '],
-          [' ', ' ', ' ', ' ', ' ', ' '],
-          [' ', ' ', ' ', ' ', ' ', ' '],
-          [1, 1, 1, 1, ' ', ' ']
-      ]);
-      expect(connect4.winState).toEqual(true)
-    });
+    expect(connect4.board).toEqual([
+      [' ', ' ', ' ', ' ', ' ', ' '],
+      [' ', ' ', ' ', ' ', ' ', ' '],
+      [' ', ' ', ' ', ' ', ' ', ' '],
+      [' ', ' ', ' ', ' ', ' ', ' '],
+      [' ', ' ', ' ', ' ', ' ', ' '],
+      [1, 1, 1, 1, ' ', ' ']
+    ])
+    expect(connect4.winState).toEqual(true)
+  })
 
-    test('horizontal win, one over', () => {
-      const connect4 = new Connect4();
+  test('horizontal win, one over', () => {
+    const connect4 = new Connect4()
 
-      connect4.selectColumn(2, 1);
-      connect4.selectColumn(3, 1);
-      connect4.selectColumn(4, 1);
-      connect4.selectColumn(5, 1);
+    connect4.selectColumn(2, 1)
+    connect4.selectColumn(3, 1)
+    connect4.selectColumn(4, 1)
+    connect4.selectColumn(5, 1)
 
-      expect(connect4.board).toEqual([
-          [' ', ' ', ' ', ' ', ' ', ' '],
-          [' ', ' ', ' ', ' ', ' ', ' '],
-          [' ', ' ', ' ', ' ', ' ', ' '],
-          [' ', ' ', ' ', ' ', ' ', ' '],
-          [' ', ' ', ' ', ' ', ' ', ' '],
-          [' ', 1, 1, 1, 1, ' ']
-      ]);
-      expect(connect4.winState).toEqual(true)
-    });
+    expect(connect4.board).toEqual([
+      [' ', ' ', ' ', ' ', ' ', ' '],
+      [' ', ' ', ' ', ' ', ' ', ' '],
+      [' ', ' ', ' ', ' ', ' ', ' '],
+      [' ', ' ', ' ', ' ', ' ', ' '],
+      [' ', ' ', ' ', ' ', ' ', ' '],
+      [' ', 1, 1, 1, 1, ' ']
+    ])
+    expect(connect4.winState).toEqual(true)
+  })
 
-    test('no horizontal win, gap in the row', () => {
-      const connect4 = new Connect4();
+  test('no horizontal win, gap in the row', () => {
+    const connect4 = new Connect4()
 
-      connect4.selectColumn(2, 1);
-      connect4.selectColumn(4, 1);
-      connect4.selectColumn(5, 1);
-      connect4.selectColumn(6, 1);
+    connect4.selectColumn(2, 1)
+    connect4.selectColumn(4, 1)
+    connect4.selectColumn(5, 1)
+    connect4.selectColumn(6, 1)
 
-      expect(connect4.board).toEqual([
-          [' ', ' ', ' ', ' ', ' ', ' '],
-          [' ', ' ', ' ', ' ', ' ', ' '],
-          [' ', ' ', ' ', ' ', ' ', ' '],
-          [' ', ' ', ' ', ' ', ' ', ' '],
-          [' ', ' ', ' ', ' ', ' ', ' '],
-          [' ', 1, ' ', 1, 1, 1]
-      ]);
-      expect(connect4.winState).toEqual(false)
-    });
-});
+    expect(connect4.board).toEqual([
+      [' ', ' ', ' ', ' ', ' ', ' '],
+      [' ', ' ', ' ', ' ', ' ', ' '],
+      [' ', ' ', ' ', ' ', ' ', ' '],
+      [' ', ' ', ' ', ' ', ' ', ' '],
+      [' ', ' ', ' ', ' ', ' ', ' '],
+      [' ', 1, ' ', 1, 1, 1]
+    ])
+    expect(connect4.winState).toEqual(false)
+  })
+})
