421fd7d4fe29760f819454eef46581bca9f67ac7
Connor Kam-Cheong <connorkc42@gmail.com>
2019-05-02 14:59:01 +0100

    Game checks for horizontal win for either player on their turn

--- src/Connect4.js
+++ src/Connect4.js
@@ -12,23 +12,28 @@ class ConnectFour {
     ]
     this.boardLength = this.board[0].length
     this.boardHeight = this.board.length
+
+    this.tokens = {
+      1: 'X',
+      2: 'O'
+    }
   }
 
   selectColumn (columnNumber, player) {
     for (let i = this.boardHeight - 1; i >= 0; i--) {
       if (this.board[i][columnNumber - 1] === ' ') {
-        this.board[i][columnNumber - 1] = player
+        this.board[i][columnNumber - 1] = this.tokens[player]
         break
       }
     }
-    this.checkHorizontalWin(columnNumber)
+    this.checkHorizontalWin(columnNumber, player)
   }
 
-  checkHorizontalWin (columnNumber) {
+  checkHorizontalWin (columnNumber, player) {
     for (let row = 0; row < this.boardHeight; row++) {
       let counter = 0
       for (let column = 0; column < this.boardLength; column++) {
-        if (this.board[row][column] === 1) {
+        if (this.board[row][column] === this.tokens[player]) {
           counter += 1
           if (counter === 4) {
             this.winState = true
